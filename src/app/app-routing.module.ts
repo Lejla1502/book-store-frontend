ac312cd3bc9cf6154117a2c2f494a697a3e04ac5
Lejla1502 <lejlasp@hotmail.com>
2022-07-21 22:00:29 +0200

    setting up the grounds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksIndexComponent } from './pages/public/books/books-index/books-index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component:BooksIndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksIndexComponent } from './pages/public/books/books-index/books-index.component';
+import { PublicHeaderComponent } from './components/public-header/public-header.component';
+import { PublicFooterComponent } from './components/public-footer/public-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksIndexComponent,
+    PublicHeaderComponent,
+    PublicFooterComponent
   ],
   imports: [
     BrowserModule,
